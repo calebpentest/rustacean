de2c0208f4e230bca3481a7cbf82987dbf77bdbf
calebpentest <calebepentest@gmail.com>
2025-05-13 10:24:14 +0100

    rust update new

--- index.rs
+++ index.rs
@@ -11,6 +11,10 @@ println!("Result is: {}", _x);
 let y: i32 = add(4,8);
 println!("Value of y is {}", y);
 
+let weight: f64 = 70.0;
+let height: f64 = 1.82;
+let bmi = calculate_bmi(weight, height);
+println!("Your bmi is {:.2}", bmi)
 }
 
 fn get_result(name: &str){
@@ -23,6 +27,10 @@ fn human_id(name: &str, age: i32, height: f64){
 
 fn add(a:i32, b:i32) -> i32{
     a + b
+}
+
+fn calculate_bmi(weight_kg: f64, height_m: f64) -> f64{
+    weight_kg / (height_m * height_m)
 }
 
 //expression and statement
